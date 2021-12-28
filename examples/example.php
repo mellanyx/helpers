a3d5ee988e52e21a2d759f8801017eb35ca91b5a
mellanyx <starik0v2000@yandex.ru>
2021-12-28 16:20:03 +0300

    test

---
+++ examples/example.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use ST\HELPERS\HelloComposer;
+
+HelloComposer::sayHi();
---
+++ src/HelloComposer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ST\HELPERS;
+
+class HelloComposer
+{
+    public static function sayHi() {
+        echo "Hello From IT-ENTERPRISE\n";
+    }
+}
